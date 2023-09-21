d4d4ea752f5ae908cdf5ded51092a48e00edc42b
BambooBear <80750868+BambooTheBear@users.noreply.github.com>
2023-09-21 10:51:37 +0200

    removed players

--- src/utils/GameHandler.js
+++ src/utils/GameHandler.js
@@ -2,5 +2,5 @@ import { reactive } from "vue"
 
 export const game = reactive({
     playing: false,
-    players: [{ name: "Tom" }, { name: "Pufid" }, { name: "Pufid2" }, { name: "Pufid3" }],
+    players: [],
 })
